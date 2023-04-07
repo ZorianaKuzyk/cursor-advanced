2c9048d8a0d6af128b38f225ab8e09f1129073b3
ZorianaKuzyk <zoryanakyzuk9@gmail.com>
2023-04-07 18:19:37 +0300

    HW-11

---
+++ HW-11/script.js
@@ -0,0 +1,18 @@
+async function getRandomChinese(length) {
+  let sign;
+  let result = "";
+  let i = length;
+
+  while (i > 0) {
+    let promise = new Promise((decision) => {
+      setTimeout(decision, length * 50);
+    });
+    sign = Date.now().toString().slice(-5);
+    result += String.fromCharCode(sign);
+    i--;
+    await promise;
+  }
+  console.log(result);
+}
+
+getRandomChinese(4);
